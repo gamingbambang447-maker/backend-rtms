bf4be751fedd6c9fb2cfabc4af0c2fca2f4bbf44
Timothy <timothywidodo30@gmail.com>
2026-04-23 13:14:16 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const SECRET = "rtms_secret_key";
+
+// DATA USER
+const users = [
+  { username: "admin", password: "1234", companies: ["ALL"] },
+  { username: "itdept", password: "1234", companies: ["ALL"] },
+  { username: "timo", password: "0000", companies: ["Seven Offshore Indonesia"] }
+];
+
+// DATA COMPANY
+const companies = [
+  "PT Dutaraya Tataperkasa",
+  "Seven Offshore Indonesia",
+  "PT Lautan Energy",
+  "PT Samudra Teknik"
+];
+
+// LOGIN
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    u => u.username === username && u.password === password
+  );
+
+  if (!user) {
+    return res.status(401).json({ message: "Login gagal" });
+  }
+
+  const token = jwt.sign(
+    { username: user.username, companies: user.companies },
+    SECRET,
+    { expiresIn: "1h" }
+  );
+
+  res.json({ message: "Login sukses", token });
+});
+
+// GET COMPANY
+app.get("/companies", (req, res) => {
+  const auth = req.headers.authorization;
+  if (!auth) return res.sendStatus(403);
+
+  const token = auth.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+
+    let allowedCompanies;
+
+    if (decoded.companies.includes("ALL")) {
+      allowedCompanies = companies;
+    } else {
+      allowedCompanies = companies.filter(c =>
+        decoded.companies.includes(c)
+      );
+    }
+
+    res.json({
+      username: decoded.username,
+      companies: allowedCompanies
+    });
+
+  } catch {
+    res.sendStatus(401);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server jalan di http://localhost:3000");
+});
